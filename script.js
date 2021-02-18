6e4cf9cdb838ea2ecd40a26c20b2e4b03f3cb4db
ProRoger <roger.e.cerpa@gmail.com>
2021-02-17 23:23:05 -0500

    started js code to track clicks

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const buttons = document.querySelectorAll('.btn')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        countClick()
+
+    })
+}) 
+
+const linkClicks = 0
+
+function countClick() {
+
+}
